b422d86ad21f2e0affdf9ac462cab5ae45840c3c
hashiguchi <hashiguchi@irvs.ait.kyushu-u.ac.jp>
2014-10-16 10:36:24 +0900

    modify demo program

--- tms_rs/tms_rs_greeting_demo/src/tms_rs_motion.cpp
+++ tms_rs/tms_rs_greeting_demo/src/tms_rs_motion.cpp
@@ -6,9 +6,11 @@
  */
 #include <ros/ros.h>
 #include <string>
+#include <iostream>
 
 #include <tms_msg_rc/smartpal_control.h>
 
+#define UNIT_VEHICLE            1
 #define UNIT_ARM_R              2
 #define UNIT_ARM_L              3
 #define UNIT_LUMBA              6
@@ -35,8 +37,12 @@ bool sp_control_function(int unit, int cmd, int arg_size, double* arg) {
 }
 
 bool sp5_control(int num, const double* arg) {
-
-	if (num==1 || num==2 || num==4) {
+// num 0:vehicle&Rarm, 1:Rarm, 2:Rarm&waist, 3:waist, 4:LRarm, 5:LRarm
+	if (num==0) {
+		double arg_vehicle[3] = {0, 0, arg[0]};
+		sp_control_function(UNIT_VEHICLE, CMD_MOVE_REL, 3, arg_vehicle);
+	}
+	if (num==0 || num==1 || num==2 || num==4 || num==5) {
 		double arg_armR[8] = {arg[3], arg[4], arg[5], arg[6], arg[7], arg[8], arg[9], 30.0};
 		sp_control_function(UNIT_ARM_R, CMD_MOVE_ABS, 8, arg_armR);
 	}
@@ -61,30 +67,64 @@ int main(int argc, char **argv)
 
 	motion_client = n.serviceClient<tms_msg_rc::smartpal_control>("sp5_control");
 
-	double motion_ini[19] = {0.0, 0.0, 0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-	double motion0[19] = {0.0, 0.0, 0.0, 1.1, -14.3, 56.6, 120.5, 0.0, 23.5, 0.0, 0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-	double motion5[19] = {0.0, -6.0, 30.0, 4.7, -45.2, -49.0, 120.5, 23.5, 15.5, -22.4, 0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-	double motion6[19] = {0.0, 0.0, 0.0, 4.7, -45.2, -49.0, 120.5, 23.5, 15.5, -22.4, 0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-	double motion7[19] = {0.0, 0.0, 0.0, 0.0, -22.2, 41.4, 40.0, 64.3, 5.5, 0.0, 0.0, 0.0, -22.2, 41.4, 40.0, 64.3, 5.5, 0.0, 0.0};
-	double motion9[19] = {0.0, 0.0, 0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 55.7, -25.5, 23.5, 115.0, 77.0, 0.0, -30.0, 0.0};
+	const double motion_ini[19] = {0.0, 0.0, 0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+	const double motion0[19] = {0.0, 0.0, 0.0, 1.1, -14.3, 56.6, 120.5, 0.0, 23.5, 0.0, 0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+	const double motion5[19] = {0.0, -6.0, 30.0, 4.7, -45.2, -49.0, 120.5, 23.5, 15.5, -22.4, 0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+	const double motion6[19] = {0.0, 0.0, 0.0, 4.7, -45.2, -49.0, 120.5, 23.5, 15.5, -22.4, 0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+	const double motion7[19] = {0.0, 0.0, 0.0, 0.0, -22.2, 41.4, 40.0, 64.3, 5.5, 0.0, 0.0, 0.0, -22.2, 41.4, 40.0, 64.3, 5.5, 0.0, 0.0};
+	const double motion9[19] = {0.0, 0.0, 0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 55.7, -25.5, 23.5, 115.0, 77.0, 0.0, -30.0, 0.0};
+	const double motion12[19] = {-126.0, 0.0, 0.0, 0.0, -24.0, 101.0, 35.8, 65.5, 0.0, -17.7, 0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0}; //Open,Close
+	const double motion13[19] = {126.0, 0.0, 0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
-	// "Hello, I am Smart pal five."
-	if (sp5_control(1, motion0)) ROS_INFO("succeed to call service motion0");
+	while(ros::ok()) {
+		int motion_num;
 
-	// "Thank you for coming to our laboratory today."
-	if (sp5_control(2, motion5)) ROS_INFO("succeed to call service motion5");
-	if (sp5_control(2, motion6)) ROS_INFO("succeed to call service motion6");
+		while(1) {
+			std::cout << "motionNum 0:Init, 1:Hello, 2:ThankYou, 3:PleaseEnjoy,"
+					" 4:DoMyBest, 5:Open(Close)Door, 10000:quit" << std::endl;
+			std::cin >> motion_num;
 
-	// "Please Enjoy watching my demonstration."
-	if (sp5_control(4, motion7)) ROS_INFO("succeed to call service motion7");
+			if(motion_num == 10000) goto QUIT;
+			if (!std::cin.fail() && (0<=motion_num && motion_num<=5)) break;
 
-	// "I will do my best."
-	if (sp5_control(4, motion9)) ROS_INFO("succeed to call service motion9");
+			std::cin.clear();
+			std::cin.ignore(1024, '\n');
+		}
 
-	//initial
-	if (sp5_control(5, motion_ini)){};
+		switch (motion_num) {
+		case 0:
+			//initial
+			if (sp5_control(5, motion_ini)){};
+			break;
+		case 1:
+			// "Hello, I am Smart pal five."
+			if (sp5_control(1, motion0)) ROS_INFO("succeed to call service motion0");
+			break;
+		case 2:
+			// "Thank you for coming to our laboratory today."
+			if (sp5_control(2, motion5)) ROS_INFO("succeed to call service motion2_1");
+			if (sp5_control(2, motion6)) ROS_INFO("succeed to call service motion2_2");//return waist
+			break;
+		case 3:
+			// "Please Enjoy watching my demonstration."
+			if (sp5_control(4, motion7)) ROS_INFO("succeed to call service motion3");
+			break;
+		case 4:
+			// "I will do my best."
+			if (sp5_control(4, motion9)) ROS_INFO("succeed to call service motion4");
+			break;
+		case 5:
+			// "Open(Close) the door."
+			if (sp5_control(4, motion12)) ROS_INFO("succeed to call service motion5_1");
+			ros::Duration(3.0).sleep();
+			if (sp5_control(4, motion13)) ROS_INFO("succeed to call service motion5_2");
 
-	return 0;
+			break;
+		default:
+			break;
+		}
+	}
+QUIT:	return 0;
 }
 
 
--- tms_rs/tms_rs_greeting_demo/src/tms_rs_speech.cpp
+++ tms_rs/tms_rs_greeting_demo/src/tms_rs_speech.cpp
@@ -8,23 +8,36 @@
 #include <ros/ros.h>
 #include <string>
 #include <vector>
+#include <iostream>
 
 #include <tms_msg_rc/robot_tts.h>
 
 ros::ServiceClient speech_client;
 
 
-bool sp5_tts(std::string* sentene, double* timing, int size) {
+//bool sp5_tts(std::string* sentene, double* timing, int size) {
+//	tms_msg_rc::robot_tts sp_tts_srv;
+//
+//	for (int i=0; i<size; i++) {
+//		sp_tts_srv.request.text = sentene[i];
+//		if (speech_client.call(sp_tts_srv)) {ROS_INFO("result: %d", sp_tts_srv.response.result);}
+//		else {
+//			ROS_ERROR("Failed to call service smartpal5_tts");
+//			return false;
+//		}
+//		ros::Duration(timing[i]).sleep();
+//	}
+//	return true;
+//}
+
+bool sp5_tts(std::string sentene) {
 	tms_msg_rc::robot_tts sp_tts_srv;
 
-	for (int i=0; i<size; i++) {
-		sp_tts_srv.request.text = sentene[i];
-		if (speech_client.call(sp_tts_srv)) {ROS_INFO("result: %d", sp_tts_srv.response.result);}
-		else {
-			ROS_ERROR("Failed to call service smartpal5_tts");
-			return false;
-		}
-		ros::Duration(timing[i]).sleep();
+	sp_tts_srv.request.text = sentene;
+	if (speech_client.call(sp_tts_srv)) {ROS_INFO("result: %d", sp_tts_srv.response.result);}
+	else {
+		ROS_ERROR("Failed to call service smartpal5_tts");
+		return false;
 	}
 	return true;
 }
@@ -35,13 +48,56 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
 	speech_client = n.serviceClient<tms_msg_rc::robot_tts>("smartpal5_tts");
 
-	int size = 4;
-	//"Hello, I am Smart pal five."
-	std::string s_sentence_list[4] = {"Hello, I am Smart pal five.", "Thank you for coming to our rabo.",
-			"Please enjoy watching my demo.", "I will do my best."};
-	double timing_list[4] = {9.0, 9.0, 7.0, 3.0};
+//	int size = 4;
+//	std::string s_sentence_list[4] = {"Hello, I am Smart pal five.", "Thank you for coming to our rabo.",
+//			"Please enjoy watching my demo.", "I will do my best."};
+//	double timing_list[4] = {9.0, 9.0, 7.0, 3.0};
+//
+//	sp5_tts(s_sentence_list, timing_list, size);
 
-	sp5_tts(s_sentence_list, timing_list, size);
+	const std::string hello("Hello, I am Smart pal five.");
+	const std::string thank_you("Thank you for coming to our rabo.");
+	const std::string enjoy("Please enjoy watching my demo.");
+	const std::string do_my_best("I will do my best.");
+	const std::string open("Open the door.");
+	const std::string close("Close the door.");
 
-	return 0;
+	while(ros::ok()) {
+		int speech_num;
+		while(1) {
+			std::cout << "speechNum 1:Hello, 2:ThankYou, 3:PleaseEnjoy, "
+					"4:DoMyBest, 5:OpenDoor, 6:CloseDoor, 10000:quit" << std::endl;
+			std::cin >> speech_num;
+
+			if(speech_num == 10000) goto QUIT;
+			if (!std::cin.fail() && (1<=speech_num && speech_num<=6)) break;
+
+			std::cin.clear();
+			std::cin.ignore(1024, '\n');
+		}
+
+		switch (speech_num) {
+		case 1:
+			if (sp5_tts(hello)) ROS_INFO("succeed to call service speech1");
+			break;
+		case 2:
+			if (sp5_tts(thank_you)) ROS_INFO("succeed to call service speech2");
+			break;
+		case 3:
+			if (sp5_tts(enjoy)) ROS_INFO("succeed to call service speech3");
+			break;
+		case 4:
+			if (sp5_tts(do_my_best)) ROS_INFO("succeed to call service speech4");
+			break;
+		case 5:
+			if (sp5_tts(open)) ROS_INFO("succeed to call service speech5");
+			break;
+		case 6:
+			if (sp5_tts(close)) ROS_INFO("succeed to call service speech6");
+			break;
+		default:
+			break;
+		}
+	}
+QUIT:	return 0;
 }
