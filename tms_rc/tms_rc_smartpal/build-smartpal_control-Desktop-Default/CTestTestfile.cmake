84dcb8ad02e594187311c7b5aead5bc73b8d5eee
robotpilot <passionvirus@gmail.com>
2014-10-17 18:31:04 +0900

    deleted make-related files

--- tms_rc/tms_rc_smartpal/build-smartpal_control-Desktop-Default/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rt/catkin_ws/src/ros_tms/tms_rc/tms_rc_smartpal/smartpal_control
-# Build directory: /home/rt/catkin_ws/src/ros_tms/tms_rc/tms_rc_smartpal/build-smartpal_control-Desktop-Default
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
