1056713467d82c30a43e1110e161719f3f23d4f3
robotpilot <passionvirus@gmail.com>
2014-11-01 15:55:48 +0900

    add feature for making the collision map

---
+++ tms_rp/tms_rp_action/include/tms_rp_collision_map.h
@@ -0,0 +1,37 @@
+#ifndef TMS_RP_COLLISION_MAP_H
+#define TMS_RP_COLLISION_MAP_H
+
+#include <tms_rp_bar.h>
+
+namespace tms_rp {
+
+class CollisionTarget
+{
+public:
+  CollisionTarget(BodyItemPtr bodyItem);
+  const string& name() { return bodyItemCollisionTarget->name(); }
+
+  cnoid::BodyItemPtr bodyItemCollisionTarget;
+  cnoid::Link *base;
+};
+
+class TmsRpCollisionMap : public boost::signals::trackable
+{
+public:
+  double x_llimit, x_ulimit, y_llimit, y_ulimit, cell_size;
+  vector<ColdetLinkPairPtr> collisionMapPairs;
+  CollisionTarget* collisionTarget;
+  cnoid::Link* collisionTargetBase() { return collisionTarget->base; }
+  TmsRpCollisionMap();
+  static TmsRpCollisionMap* instance();
+  virtual ~TmsRpCollisionMap();
+  bool makeCollisionMap(vector<vector<int> >& out_collision_map);
+  void SetCollisionTarget(BodyItemPtr bodyItem);
+private:
+  void initialCollision();
+  bool isColliding();
+};
+
+}
+
+#endif // TMS_RP_COLLISION_MAP_H
