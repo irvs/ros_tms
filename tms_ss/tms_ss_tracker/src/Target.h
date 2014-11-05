ec078bf1f027848196cc8cdaef6d55172788066d
Yuuta-Watanabe <ars4253a@gmail.com>
2014-11-05 23:47:14 +0900

    add tms_ss_tracker

---
+++ tms_ss/tms_ss_tracker/src/Target.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// CTarget コマンド ターゲット
+
+class CTarget
+{
+public:
+	CTarget();
+	virtual ~CTarget();
+
+public:
+	int id;
+	double px;
+	double py;
+	int cnt;
+
+public:
+	void SetPosi(double x, double y);
+};
+
+
