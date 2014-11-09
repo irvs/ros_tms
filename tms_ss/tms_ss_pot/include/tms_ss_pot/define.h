388e95d4f486775458debf7e48f54f09c6e2542a
Yuuta-Watanabe <ars4253a@gmail.com>
2014-11-10 00:43:23 +0900

    modefied tms_ss_pot

--- tms_ss/tms_ss_pot/include/tms_ss_pot/define.h
+++ tms_ss/tms_ss_pot/include/tms_ss_pot/define.h
@@ -1,11 +1,27 @@
-//----------------------------------------------------------
-// @file   : define.h
-// @author : Watanabe Yuuta
-// @version: Ver0.0.1 (since 2014.05.02)
-// @date   : 2016.11.01
-//----------------------------------------------------------
-
+// host define.h
 #pragma once
+
+#define MAX_CONNECT 2
+#define PI 3.14159265358979323846
+#define MAX_DATA_SIZE_LRF 1000//726//727//1081//726//1081//727//1081//700
+#define MAX_TRACKING_OBJECT 10//99
+#define MAX_PARTICLE_NUM 2000//2000//8000
+#define MAX_PARTICLE_NUM_MCMC 1000//1000
+#define TRACKING_HISTORY 30
+#define STAGE_X 8000//11000//8000//12000//9000//12000//14000
+#define STAGE_Y 8000//11000//8000//12000//11000//18000//20000
+#define GRID_DISTANCE 200//500
+#define NEAR_POSITION 0.5 // [m]
+#define CNT_PER_TMSSEND 50
+#define MSEC_PER_TMSSEND 100//200
+#define CNT_PER_NODEIMAGESAVE 30
+#define OCCLUDED_CNT 20
+#define DISCOVER_CNT 5
+
+#define sqr(x) ((x)*(x))
+#define ssqrs(x,y) (sqr(x)+sqr(y))
+#define norm(x,y,z) sqrt(sqr(x)+sqr(y)+sqr(z))
+#define deg2rad(x) ((double)x*PI/180.0)
 
 typedef struct _LRFParam{
 	double tx;
@@ -19,3 +35,5 @@ typedef struct _LRFParam{
 	double divangle;
 } LRFParam;
 
+#define ZeroMemory(arg1, arg2) memset(arg1, 0, arg2)
+#define CopyMemory(arg1, arg2, arg3) memcpy(arg1, arg2, arg3)
