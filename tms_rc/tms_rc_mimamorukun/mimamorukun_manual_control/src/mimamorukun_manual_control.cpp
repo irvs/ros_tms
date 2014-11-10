42b429b98c86b4cc557faf8bef7ea262d2e24fd0
Akio Shigekane <shigekane@irvs.ait.kyushu-u.ac.jp>
2014-11-10 14:33:11 +0900

    * change speed ang sigma ddata struct to Geometry_msgs::Twist * refactor updateOdom()

--- tms_rc/tms_rc_mimamorukun/mimamorukun_manual_control/src/mimamorukun_manual_control.cpp
+++ tms_rc/tms_rc_mimamorukun/mimamorukun_manual_control/src/mimamorukun_manual_control.cpp
@@ -35,17 +35,13 @@ long int 	ENC_L = 0;
 long int 	ENC_R = 0;
 int 	POS_X = 0;
 int 	POS_Y = 0;
-double 	POS_SIGMA = 0;
-double 	POS_ANG = 0;
+// double 	POS_SIGMA = 0;
+// double 	POS_ANG = 0;
 
-int spd_raw_L   = 0;
-int spd_raw_R   = 0;
-double spd_mm_L = 0;
-double spd_mm_R = 0;
 int spd_cmd_L = 0;
 int spd_cmd_R = 0;
-double joy_cmd_spd = 0.0;
-double joy_cmd_turn = 0.0;
+/*double joy_cmd_spd = 0.0;
+double joy_cmd_turn = 0.0;*/
 
 ros::ServiceClient db_client;
 
@@ -57,6 +53,7 @@ public:
     void updateOdom();
     void updateVicon();
     void updateCompFilter();
+    geometry_msgs::Twist    tgtTwist;
     geometry_msgs::Pose2D   pos_odom;
     geometry_msgs::Pose2D   pos_vicon;
     geometry_msgs::Pose2D   pos_fusioned;
@@ -99,70 +96,55 @@ void MachinePose_s::updateOdom(){
     long int tmpENC_R = 0;
     string reply;
     client_socket << "@GP1@GP2";    /*use 250ms for send and get reply*/
-/*    client_socket >> reply;          
-    cout << "Response:" << reply << "\n";*/
-    //sscanf(reply.c_str(),"@GP1,%ld@GP2,%ld",&tmpENC_L,&tmpENC_R);
+    client_socket >> reply;          
+    cout << "Response:" << reply << "\n";
+    sscanf(reply.c_str(),"@GP1,%ld@GP2,%ld",&tmpENC_L,&tmpENC_R);
     if(tmpENC_L > ENC_MAX/2)ENC_L = tmpENC_L-(ENC_MAX+1);
     else                    ENC_L = tmpENC_L;
     if(tmpENC_R > ENC_MAX/2)ENC_R = tmpENC_R-(ENC_MAX+1);
     else                    ENC_R = tmpENC_R;
 
 	static long int ENC_R_old = 0;static long int ENC_L_old = 0;
-	double detLp , r , dX ,dY,dL;
+	double detLp/*,r , dX ,dY,dL*/;
 
 	//エンコーダーの位置での前回からの移動距離dL_R,dL_Lを算出
 	double dL_L = (double)(ENC_L-ENC_L_old)*(-DIST_PER_PULSE);
     double dL_R = (double)(ENC_R-ENC_R_old)*  DIST_PER_PULSE;
 
-///////////////////////////////////////////////////////////
-    ////////////////////for debuggind//////////////
-    spd_raw_L = ENC_L-ENC_L_old;
-    spd_raw_R = ENC_R-ENC_R_old;
-    spd_mm_L = dL_L;
-    spd_mm_R = dL_R;
-
 	//角速度SIGMAを算出
-	//POS_SIGMA = ((ENC_R-ENC_R_old)*DIST_PER_PULSE - (ENC_L-ENC_L_old)*(-DIST_PER_PULSE))/ENCDIST;
-	POS_SIGMA = (dL_R - dL_L)/WHEEL_DIST;
-	dL = (dL_R + dL_L)*0.50000;
+	double POS_SIGMA = (dL_R - dL_L)/WHEEL_DIST;
+	double dL = (dL_R + dL_L)*0.50000;
 
 	//移動距離deLpを算出
 	if(fabs(POS_SIGMA)<0.0001){//左右の速度が等しく直進するとき
 		POS_SIGMA = 0.0;
 		detLp = dL_R;
 	}else{                       //左右どちらかにマシンの向きが変わってるとき
-		r = dL/POS_SIGMA;
-		detLp = 2.0000*r*sin(POS_SIGMA*0.50000);
+		// r = dL/POS_SIGMA;
+		detLp = 2.0000*(dL/POS_SIGMA)*sin(POS_SIGMA*0.50000);
 	}
 
-/*    mchn_pose.vel_odom.x = detLp * cos(POS_ANG + (POS_SIGMA/2.0));
-    mchn_pose.vel_odom.y = detLp * sin(POS_ANG + (POS_SIGMA/2.0));*/
-	dX = detLp * cos(POS_ANG + (POS_SIGMA/2.0));//X,Yの前回からの移動量計算
-	dY = detLp * sin(POS_ANG + (POS_SIGMA/2.0));
+/*    mchn_pose.vel_odom.x = detLp * cos(mchn_pose.pos_odom.theta + (POS_SIGMA/2.0));
+    mchn_pose.vel_odom.y = detLp * sin(mchn_pose.pos_odom.theta + (POS_SIGMA/2.0));*/
+	double dX = detLp * cos(mchn_pose.pos_odom.theta + (POS_SIGMA/2.0));//X,Yの前回からの移動量計算
+	double dY = detLp * sin(mchn_pose.pos_odom.theta + (POS_SIGMA/2.0));
 	ENC_R_old = ENC_R;//前回のエンコーダーの値を記録
 	ENC_L_old = ENC_L;
-	POS_ANG = (ENC_R_old - (-ENC_L_old))*DIST_PER_PULSE/WHEEL_DIST;//現在の角度を算出
+	mchn_pose.pos_odom.theta = (ENC_R_old - (-ENC_L_old))*DIST_PER_PULSE/WHEEL_DIST;//現在の角度を算出
 
-	while(POS_ANG > 3.14159265){  //向いている角度を-180°~180°(-π~π)の範囲に合わせる
-		POS_ANG = POS_ANG - (2*3.14159265);
+	while(mchn_pose.pos_odom.theta > 3.14159265){  //向いている角度を-180°~180°(-π~π)の範囲に合わせる
+		mchn_pose.pos_odom.theta = mchn_pose.pos_odom.theta - (2*3.14159265);
 	}
-	while(POS_ANG < -3.14159265){
-		POS_ANG = POS_ANG + (2*3.14159265);
+	while(mchn_pose.pos_odom.theta < -3.14159265){
+		mchn_pose.pos_odom.theta = mchn_pose.pos_odom.theta + (2*3.14159265);
 	}
 
-	//data->ddR = dL_R - data->dR;//構造体に値を入力
-	//data->ddL = dL_L - data->dL;
-	//data->dR = dL_R;
-	//data->dL = dL_L;
 	mchn_pose.pos_odom.x += dX;
     mchn_pose.pos_odom.y += dY;
-    mchn_pose.pos_odom.theta = POS_ANG;
+    // mchn_pose.pos_odom.theta = POS_ANG;
     mchn_pose.vel_odom.x =  dX * ROS_RATE;
     mchn_pose.vel_odom.y =  dY * ROS_RATE;
     mchn_pose.vel_odom.theta = (dL_R - (-dL_L))/WHEEL_DIST;
-
-/*    POS_X += dX;
-	POS_Y += dY;*/
     return ;
 }
 
@@ -178,8 +160,10 @@ void init(){
  *   arg_speed: forward speed   [mm/sec]
  *   arg_theta: CCW turn speed  [radian/sec]
  * ----------------------------------*/
-void move(double arg_speed, double arg_theta){
-    //ROS_INFO("X:%2.f   Theta:%2.f",arg_speed,arg_theta);
+void move(/*double arg_speed, double arg_theta*/){
+    double arg_speed = mchn_pose.tgtTwist.linear.x;
+    double arg_theta = mchn_pose.tgtTwist.angular.z;
+    ROS_INFO("X:%4.2f   Theta:%4.2f",arg_speed,arg_theta);
     double val_L = -Dist2Pulse(arg_speed) + Dist2Pulse((WHEEL_DIST/2)*arg_theta);
     double val_R =  Dist2Pulse(arg_speed) + Dist2Pulse((WHEEL_DIST/2)*arg_theta);
     val_L = (int)Limit(val_L,(double)SPEED_MAX,(double)-SPEED_MAX);
@@ -194,27 +178,25 @@ void move(double arg_speed, double arg_theta){
     string cmd_R = boost::lexical_cast<string>(val_R);
 
     string message = "@SS1," + cmd_L + "@SS2," + cmd_R;
-//    string message = "@GP1@GP2@SS1," + cmd_L + "@SS2," + cmd_R;
     string reply;
     client_socket << message;
-/*    client_socket >> reply;
-    cout << "Response:" << reply << "\n";*/
-
-/*   client_socket << "@GS1@GS2";
-   client_socket >> reply;
-   cout << "Response:" << reply << "\n";*/
+    client_socket >> reply;
+    cout << "Response:" << reply << "\n";
 }
 
 
 void receiveCmdVel(const geometry_msgs::Twist::ConstPtr& cmd_vel){
-    move(cmd_vel->linear.x,cmd_vel->angular.z);
+    mchn_pose.tgtTwist = *cmd_vel;
+    move(/*cmd_vel->linear.x,cmd_vel->angular.z*/);
 }
 
 void receiveJoy(const sensor_msgs::Joy::ConstPtr& joy){
     ROS_INFO("Rrecieve joy");
     // move(joy->axes[1]*300100,joy->axes[3]*1/*10*/);
-    joy_cmd_spd = joy->axes[1]*300;//600;
-    joy_cmd_turn = joy->axes[3]*0.7;//1;
+    mchn_pose.tgtTwist.linear.x =   joy->axes[1]*300;//600;
+    mchn_pose.tgtTwist.angular.z =  joy->axes[3]*0.7;//1;
+    // joy_cmd_spd = joy->axes[1]*300;//600;
+    // joy_cmd_turn = joy->axes[3]*0.7;//1;
 }
 
 
@@ -262,17 +244,17 @@ int main(int argc, char **argv){
     ros::Rate   r(ROS_RATE);
     while(n.ok()){
         // ROS_INFO("");
-        move(joy_cmd_spd,joy_cmd_turn);
-        mchn_pose.updateVicon();
-        //mchn_pose.updateOdom();
-        ROS_INFO("x:%4.2lf y:%4.2lf th:%4.2lf",
+        move(/*joy_cmd_spd,joy_cmd_turn*/);
+        //mchn_pose.updateVicon();
+        mchn_pose.updateOdom();
+/*        ROS_INFO("x:%4.2lf y:%4.2lf th:%4.2lf",
             mchn_pose.pos_vicon.x,
             mchn_pose.pos_vicon.y,
-            mchn_pose.pos_vicon.theta);
-
-/*        ROS_INFO("L::cmd:%6d  raw:%6d mm:%lf    R::cmd:%6d  raw:%6d mm:%lf"
-            ,spd_cmd_L,1*spd_raw_L,1*spd_mm_L
-            ,spd_cmd_R,1*spd_raw_R,1*spd_mm_R);*/
+            mchn_pose.pos_vicon.theta);*/
+        ROS_INFO("x:%4.2lf y:%4.2lf th:%4.2lf",
+            mchn_pose.pos_odom.x,
+            mchn_pose.pos_odom.y,
+            Rad2Deg(mchn_pose.pos_odom.theta));
         last_time = current_time;
         current_time = ros::Time::now();
         ros::spinOnce();
