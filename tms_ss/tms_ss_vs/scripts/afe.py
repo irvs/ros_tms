5b3374cca52f4ea889369a25c6797a6fa003f325
Akio Shigekane <shigekane@irvs.ait.kyushu-u.ac.jp>
2015-03-04 12:57:57 +0900

    * delete "\n\r" in in_js * fix key name * add exception now run well

--- tms_ss/tms_ss_vs/scripts/afe.py
+++ tms_ss/tms_ss_vs/scripts/afe.py
@@ -9,18 +9,17 @@
 import rospy
 import serial
 import json
+import traceback
 import pprint
 import subprocess
 
-from std_msgs.msg import Float32
 from std_msgs.msg import Int32
 
-
 PORT = "/dev/ttyUSB0"
-KEYS = ("raw", "PRI")
+KEYS = ("Raw", "HBR")
 
 dev = serial.Serial(
-    # port=PORT,
+    port=PORT,
     baudrate=115200,
     )
 
@@ -28,20 +27,24 @@ dev = serial.Serial(
 def main():
     print "Hello World"
     pub = {}
-    pub[KEYS[0]] = rospy.Publisher('afe/blood_pressue', Float32, queue_size=10)
-    pub[KEYS[1]] = rospy.Publisher('afe/period', Int32, queue_size=10)
+    pub[KEYS[0]] = rospy.Publisher('afe/blood_pressue', Int32, queue_size=10)
+    pub[KEYS[1]] = rospy.Publisher('afe/hbr', Int32, queue_size=10)
     rospy.init_node("afe", anonymous=False)
     # print subprocess.check_output("rqt_plot /afe/blood_pressue &".split(" "))
     # print subprocess.check_output("rqt_plot /afe/period &".split(" "))
     r = rospy.Rate(50)
     while not rospy.is_shutdown():
         r.sleep()
-        print "readling"
-        in_js = dev.readline()
-        print "in:", in_js
-        in_dic = json.dumps(in_js)
+        print ""
+        in_js = dev.readline().replace('\n\r','')
+        print type(in_js),"   in:", in_js
+        in_dic = {}
+        try:
+            in_dic = json.loads(in_js)
+        except:
+            print traceback.format_exc()
         pprint.pprint(in_dic)
-        for key, val in in_dic:
+        for key, val in in_dic.items():
             pub[key].publish(val)
         pass
 
