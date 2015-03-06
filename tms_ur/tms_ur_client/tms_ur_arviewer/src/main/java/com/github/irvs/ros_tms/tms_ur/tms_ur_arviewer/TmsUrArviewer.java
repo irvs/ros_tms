cd078dc80d4cf57ce3b709e85e3e277750b1758c
Kazuto <k_nakashima@irvs.ait.kyushu-u.ac.jp>
2015-03-06 19:25:54 +0900

    added tms_ur_client/tms_ur_dbbrowser

---
+++ tms_ur/tms_ur_client/tms_ur_arviewer/src/main/java/com/github/irvs/ros_tms/tms_ur/tms_ur_arviewer/TmsUrArviewer.java
@@ -0,0 +1,15 @@
+package com.github.irvs.ros_tms.tms_ur.tms_ur_arviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TmsUrArviewer extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
