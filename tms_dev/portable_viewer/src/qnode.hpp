daee4734d5322c4d4413b56bb7f6370a3160f31e
Yuuta-Watanabe <ars4253a@gmail.com>
2015-07-25 01:08:43 +0900

    old portable viewer for backup

---
+++ tms_dev/portable_viewer/src/qnode.hpp
@@ -0,0 +1,36 @@
+//------------------------------------------------------------------------------
+// @file   : qnode.hpp
+// @brief  : rosnode function
+// @author : Watanabe Yuuta
+// @version: Ver0.1.1
+// @date   : 2015.4.1
+//------------------------------------------------------------------------------
+#ifndef NODE_HPP
+#define NODE_HPP
+
+//------------------------------------------------------------------------------
+#include <QStringListModel>
+#include <QThread>
+
+#include <string>
+
+#include <ros/ros.h>
+
+//------------------------------------------------------------------------------
+class QNode : public QThread
+{
+Q_OBJECT
+
+public:
+    int init_argc;
+    char** init_argv;
+    const std::string node_name;
+
+    QNode(int argc, char** argv, const std::string &name );
+    virtual ~QNode();
+
+};
+
+#endif // NODE_HPP
+
+//------------------------------------------------------------------------------
