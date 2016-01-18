d9f28a7f092483ce3858239bf11e1731252e2e71
Masatomo-Onishi <onishi@irvs.ait.kyushu-u.ac.jp>
2016-01-19 05:02:05 +0900

    tms_ss_kinect_v2:commit for db_replayer #2

--- tms_ss/tms_ss_kinect_v2/scripts/tms_db_replay.py
+++ tms_ss/tms_ss_kinect_v2/scripts/tms_db_replay.py
@@ -30,32 +30,41 @@ class TmsDbReplayer():
         self.sendDbHistoryInformation()
 
     def sendDbHistoryInformation(self):
-        rate = rospy.Rate(10)
+        # Input
+        id_array = range(1006, 1019)
+        rate = rospy.Rate(100)
+        d1 = datetime.now()-timedelta(days=4,hours=0)
+        d2 = d1+timedelta(days=0,hours=1,minutes=0)
+        t1 = 'ISODate('+d1.isoformat()+')'
+        t2 = 'ISODate('+d2.isoformat()+')'
+        play_speed = 600
 
-        while not rospy.is_shutdown():
-            temp_dbdata = Tmsdb()
-            object_information = TmsdbStamped()
+        # Extract data from DB
+        temp_dbdata = Tmsdb()
+        object_information = TmsdbStamped()
+        rospy.loginfo('Extracting data: from {0} to {1}'.format(t1,t2))
+        db.history.create_index([('time', pymongo.ASCENDING), ('id', pymongo.ASCENDING)])
 
-            d = datetime.now()-timedelta(days=3,hours=11);
-            t1 = 'ISODate('+d.isoformat()+')'
-            t2 = 'ISODate('+(d+timedelta(days=0,hours=1,minutes=0)).isoformat()+')'
-            rospy.loginfo('Extracting data: from {0} to {1}'.format(t1,t2))
-            cursor = db.history.find({'id':{'$gte':1006, '$lte':1018}, 'time':{'$gte':t1,'$lt':t2}, 'state':1}).sort('time')
-            cursor_size = cursor.count()  # I don't know why this sequence needs much time.
-            rospy.loginfo('Arranging data ...:')
-            prog_count = 0
+        # Publish data
+        rospy.loginfo('Start to publish data.')
+        prev_time = d1
+        t_delta = timedelta(milliseconds=10*play_speed)
+        curr_time = d1 + t_delta
+        while rospy.is_shutdown() or curr_time <= d2:
+            t1 = 'ISODate('+prev_time.isoformat()+')'
+            t2 = 'ISODate('+curr_time.isoformat()+')'
+            rospy.loginfo('from {0} to {1}'.format(t1,t2))
+            cursor = db.history.find({'id':{'$gte':1006, '$lte':1018}, 'time':{'$gte':t1,'$lt':t2}, 'state':1})
             for doc in cursor:
                 del doc['_id']
                 temp_dbdata = db_util.document_to_msg(doc, Tmsdb)
                 object_information.tmsdb.append(temp_dbdata)
-                if len(object_information.tmsdb) >= cursor_size/10 * prog_count:
-                    rospy.loginfo('Progress:[{0}%]'.format(prog_count*10))
-                    prog_count += 1
-            rospy.loginfo('Start to publish data.')
             self.data_pub.publish(object_information)
-            rospy.loginfo('Finish publishing data.')
-
+            prev_time = curr_time
+            curr_time += t_delta
             rate.sleep()
+
+        rospy.loginfo('Finish publishing data.')
 
     def shutdown(self):
         rospy.loginfo("Stopping the node")
