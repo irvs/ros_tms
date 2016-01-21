67025cdbc8b1a0776139707149cd44211bb43f1f
Masatomo-Onishi <onishi@irvs.ait.kyushu-u.ac.jp>
2016-01-21 20:43:14 +0900

    tms_ss_kinect_v2: commit for db_replayer #6

--- tms_ss/tms_ss_kinect_v2/scripts/tms_db_replay.py
+++ tms_ss/tms_ss_kinect_v2/scripts/tms_db_replay.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
+import threading
 import rospy
 import genpy
 import pymongo # https://api.mongodb.org/python/2.6.3/
@@ -9,6 +10,7 @@ from bson import json_util
 from bson.objectid import ObjectId
 from datetime import *
 from tms_msg_db.msg import TmsdbStamped, Tmsdb
+from tms_msg_db.srv import *
 import tms_db_manager.tms_db_util as db_util
 
 client = pymongo.MongoClient('localhost:27017')
@@ -25,16 +27,44 @@ class TmsDbReplayer():
         if not self.is_connected:
             raise Exception("Problem of connection")
 
-        self.sendDbHistoryInformation(range(1006, 1019), 'tms_db_replayer', '2016-01-18T04:00:00', '2016-01-18T05:00:00', 600)
+        self.thread_list = []
+        result = rospy.Service('tms_db_replayer', TmsdbCallReplayer, self.handleCallReplayer)
 
-    def sendDbHistoryInformation(self, id_array, topic_name, start_date, end_date, play_speed):
+        rospy.spin()
+
+    def handleCallReplayer(self, req):
+        rospy.loginfo('Received calling')
+
+        self.request_id = range(1006, 1019)
+        self.topic_name = 'tms_db_replayer'
+        self.start_date = '2016-01-18T04:00:00'
+        self.end_date = '2016-01-18T05:00:00'
+        self.play_speed = 60
+
+        #self.request_id = req.request_id
+        #self.topic_name = req.topic_name
+        ##self.start_date = req.start_date
+        ##self.end_date = req.end_date
+        #self.start_date = datetime.fromtimestamp(req.start_date/1e3)
+        #self.end_date = datetime.fromtimestamp(req.end_date/1e3)
+        #self.play_speed = req.play_speed
+
+        thread = threading.Thread(target=self.sendDbHistoryInformation)
+        self.thread_list.append(thread)
+        thread.daemon = True
+        thread.start()
+
+        return TmsdbCallReplayerResponse(0)
+
+    def sendDbHistoryInformation(self):
         # Input
-        d1 = datetime.strptime(start_date, '%Y-%m-%dT%H:%M:%S')
-        d2 = datetime.strptime(end_date, '%Y-%m-%dT%H:%M:%S')
+        d1 = datetime.strptime(self.start_date, '%Y-%m-%dT%H:%M:%S')
+        d2 = datetime.strptime(self.end_date, '%Y-%m-%dT%H:%M:%S')
         t1 = 'ISODate('+d1.isoformat()+')'
         t2 = 'ISODate('+d2.isoformat()+')'
 
-        self.data_pub = rospy.Publisher(topic_name, TmsdbStamped, queue_size=10)
+        self.rate = rospy.Rate(100)  # [Hz] : Keep consistence with db_publisher
+        self.data_pub = rospy.Publisher(self.topic_name, TmsdbStamped, queue_size=10)
 
         # Extract data from DB
         temp_dbdata = Tmsdb()
@@ -43,17 +73,16 @@ class TmsDbReplayer():
         db.history.create_index([('time', pymongo.ASCENDING), ('id', pymongo.ASCENDING)])
 
         # Publish data
-        rate = rospy.Rate(100)  # [Hz] : Keep consistence with db_publisher
         rospy.loginfo('Start to publish data.')
         prev_time = d1
-        t_delta = timedelta(milliseconds=10*play_speed)
+        t_delta = timedelta(milliseconds=10*self.play_speed)
         curr_time = d1 + t_delta
         while rospy.is_shutdown() or curr_time <= d2:
             t1 = 'ISODate('+prev_time.isoformat()+')'
             t2 = 'ISODate('+curr_time.isoformat()+')'
             rospy.loginfo('from {0} to {1}'.format(t1,t2))
 
-            cursor = db.history.find({'$or':[json.loads('{"id":'+str(x)+'}') for x in id_array], 'time':{'$gte':t1,'$lt':t2}, 'state':1})
+            cursor = db.history.find({'$or':[json.loads('{"id":'+str(x)+'}') for x in self.request_id], 'time':{'$gte':t1,'$lt':t2}, 'state':1})
             for doc in cursor:
                 del doc['_id']
                 temp_dbdata = db_util.document_to_msg(doc, Tmsdb)
@@ -61,7 +90,7 @@ class TmsDbReplayer():
             self.data_pub.publish(object_information)
             prev_time = curr_time
             curr_time += t_delta
-            rate.sleep()
+            self.rate.sleep()
 
         rospy.loginfo('Finish publishing data.')
 
