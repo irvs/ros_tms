0c3efa4ed5983ab1d6e2baa0d92e82a6bd5b3ffa
AkioShigekane <shigekane@irvs.ait.kyushu-u.ac.jp>
2016-06-09 17:09:24 +0900

    speedup

--- format_all.sh
+++ format_all.sh
@@ -1,8 +1,5 @@
 # /bin/sh
 
 cd $(dirname $0)
-alias beautify='clang-format-3.6 -style="{BasedOnStyle: Google, ColumnLimit: 100, IndentWidth: 2, UseTab: Never}" -i'
-find . -type f -regex ".*\.\(c\|cpp\|cxx\|h\|hpp\|hxx\)$"|while read f
-do
-    beautify $f
-done
+find . -type f -regex ".*\.\(c\|cpp\|cxx\|h\|hpp\|hxx\)$" | xargs clang-format-3.6 -i -style=file $1
+# find . -type f -regex ".*\.\(c\|cpp\|cxx\|h\|hpp\|hxx\)$" | xargs clang-format-3.6 -i -style="{BasedOnStyle: Google, ColumnLimit: 100, IndentWidth: 2, UseTab: Never}"
