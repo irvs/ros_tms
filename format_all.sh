54e5c6404f333ee0f09cb0180834b38a9d3ff60a
Akio Shigekane <motorado519+git@gmail.com>
2016-06-15 20:13:48 +0900

    [add] install formatter if they are not found

--- format_all.sh
+++ format_all.sh
@@ -1,5 +1,16 @@
 # /bin/sh
 
+if ! type clang-format-3.6;then
+  echo "installing clang-format-3.6"
+  sudo apt-get install clang-format-3.6
+fi
+
+if ! type autopep8;then
+  echo "installing autopep8"
+  sudo apt-get install python-autopep8
+fi
+
+
 cd $(dirname $0)
 find . -type f -regex ".*\.\(c\|cpp\|cxx\|h\|hpp\|hxx\)$" | xargs clang-format-3.6 -i -style=file $1
 # find . -type f -regex ".*\.\(c\|cpp\|cxx\|h\|hpp\|hxx\)$" | xargs clang-format-3.6 -i -style="{BasedOnStyle: Google, ColumnLimit: 100, IndentWidth: 2, UseTab: Never}"
