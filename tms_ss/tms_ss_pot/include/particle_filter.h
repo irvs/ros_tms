9a4942640a7fe462b136ee56e3019f1ac3d85ce5
Junya-s <sakamoto@irvs.ait.kyushu-u.ac.jp>
2016-10-11 17:57:40 +0900

    add tms_ss_pot

---
+++ tms_ss/tms_ss_pot/include/particle_filter.h
@@ -0,0 +1,38 @@
+
+#include "condens/condens.hpp"
+
+#ifndef M_PI
+#define M_PI 3.1415926535897932384626433832795
+#endif
+
+class CPF
+{
+public:
+  CPF();
+  virtual ~CPF();
+
+public:
+  void update();
+  void initialize(int area[2]);
+  void clear();
+  void SetTarget(double t[2]);
+  void GetTarget(double t[2]);
+  double state[4];
+  void SetID(int ID);
+  int GetID();
+  int GetCnt();
+
+  // protected:
+  CvConDensation* particle_filter;
+  double m_sigma;
+  int Dimension;
+  int Num_of_particles;
+  double Area[4];
+  double MaxVel;
+  double target[2];
+  int m_ID;
+  int m_cnt;
+  double Noise[2];
+
+  double likelihood(double p[2]);
+};
---
+++ tms_ss/tms_ss_pot/include/target.cpp
@@ -0,0 +1,35 @@
+// Target.cpp : ŽÀ‘•ƒtƒ@ƒCƒ‹
+//
+#ifdef WIN32
+#pragma warning(disable : 4996)
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+//#include "opencv2\opencv.hpp"
+#include "opencv/cv.h"
+
+#include "define.h"
+#include "target.h"
+
+// CTarget
+
+CTarget::CTarget()
+{
+  id = 0;
+  px = 0;
+  py = 0;
+  cnt = 0;
+}
+
+CTarget::~CTarget()
+{
+}
+
+void CTarget::SetPosi(double x, double y)
+{
+  px = x;
+  py = y;
+}
